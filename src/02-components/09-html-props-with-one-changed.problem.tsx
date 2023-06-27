a9146a6666a6d60139de952869c7fefd54fe79d5
Aashutosh Kumar <aashutoshkumar869@gmail.com>
2023-06-27 13:49:32 +0530

    completed exercise 9-10

--- src/02-components/09-html-props-with-one-changed.problem.tsx
+++ src/02-components/09-html-props-with-one-changed.problem.tsx
@@ -1,27 +1,29 @@
 import { ComponentProps } from "react";
 import { Equal, Expect } from "../helpers/type-utils";
 
-export const Input = (
-  props: ComponentProps<"input"> & { onChange: (value: string) => void }
-) => {
-  return (
-    <input
-      {...props}
-      onChange={(e) => {
-        props.onChange(e.target.value);
-      }}
-    ></input>
-  );
+interface InputProps extends Omit<ComponentProps<"input">, "onChange"> {
+	onChange: (value: string) => void;
+}
+
+export const Input = (props: InputProps) => {
+	return (
+		<input
+			{...props}
+			onChange={e => {
+				props.onChange(e.target.value);
+			}}
+		></input>
+	);
 };
 
 const Parent = () => {
-  return (
-    <Input
-      onChange={(e) => {
-        console.log(e);
+	return (
+		<Input
+			onChange={e => {
+				console.log(e);
 
-        type test = Expect<Equal<typeof e, string>>;
-      }}
-    ></Input>
-  );
+				type test = Expect<Equal<typeof e, string>>;
+			}}
+		></Input>
+	);
 };
--- src/02-components/10-extracting-props-from-custom-components.problem.tsx
+++ src/02-components/10-extracting-props-from-custom-components.problem.tsx
@@ -1,26 +1,27 @@
 // Imagine NavBar is an external library!
 
 export const NavBar = (props: {
-  title: string;
-  links: string[];
-  children: React.ReactNode;
+	title: string;
+	links: string[];
+	children: React.ReactNode;
 }) => {
-  return <div>Some content</div>;
+	return <div>Some content</div>;
 };
 
 // Your app:
 
 import { Equal, Expect } from "../helpers/type-utils";
+import { ComponentProps } from "react";
 
-type NavBarProps = unknown;
+type NavBarProps = ComponentProps<typeof NavBar>;
 
 type test = Expect<
-  Equal<
-    NavBarProps,
-    {
-      title: string;
-      links: string[];
-      children: React.ReactNode;
-    }
-  >
+	Equal<
+		NavBarProps,
+		{
+			title: string;
+			links: string[];
+			children: React.ReactNode;
+		}
+	>
 >;
