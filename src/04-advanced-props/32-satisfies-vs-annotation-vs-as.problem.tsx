1e155e80333ff9021acd1ea800c6813cdf165975
Aashutosh Kumar <aashutoshkumar869@gmail.com>
2023-06-28 15:41:25 +0530

    completed exercise 31-32

--- src/04-advanced-props/32-satisfies-vs-annotation-vs-as.problem.tsx
+++ src/04-advanced-props/32-satisfies-vs-annotation-vs-as.problem.tsx
@@ -1,14 +1,16 @@
 import { ComponentProps } from "react";
 import { Equal, Expect } from "../helpers/type-utils";
 
+type ButtonProps = ComponentProps<"button">;
+
 const buttonProps = {
-  type: "button",
-  // @ts-expect-error
-  illegalProperty: "I AM ILLEGAL",
-};
+	type: "button",
+	// @ts-expect-error
+	illegalProperty: "I AM ILLEGAL",
+} satisfies ButtonProps;
 
 <>
-  <button {...buttonProps}>Click Me!</button>
+	<button {...buttonProps}>Click Me!</button>
 </>;
 
 const buttonPropType = buttonProps.type;
